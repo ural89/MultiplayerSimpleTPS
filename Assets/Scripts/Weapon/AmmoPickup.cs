e9be97bd5ab69f53d592343fe29851fa83d5b4b6
ural89 <uralkemal@gmail.com>
2023-11-17 16:17:32 +0300

    fiiixxxxed

--- Assets/Scripts/Weapon/AmmoPickup.cs
+++ Assets/Scripts/Weapon/AmmoPickup.cs
@@ -2,32 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Fusion;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AmmoPickup : NetworkBehaviour
 {
-    [Networked(OnChanged = nameof(OnHasPickedChanged))] public NetworkBool HasPicked { get; set; } = false;
-    [SerializeField] public Renderer _renderer;
+    [Networked(OnChanged = nameof(OnHasPickedChanged))] private NetworkBool HasPicked { get; set; } = false;
+    [SerializeField] private Renderer _renderer;
 
     private static void OnHasPickedChanged(Changed<AmmoPickup> changed)
     {
-        // changed.Behaviour._renderer.enabled = false;
+        //changed.Behaviour._renderer.enabled = !changed.Behaviour.HasPicked;
+        changed.Behaviour.EnableObject(!changed.Behaviour.HasPicked);
         // Debug.Log("Picked up " + changed.Behaviour.HasPicked);
     }
-  
+    public void EnableObject(bool isEnabled)
+    {
+        gameObject.SetActive(isEnabled);
+    }
+    public override void Spawned()
+    {
+       // _renderer.enabled = !HasPicked;
+    }
     public void OnPicked(Player player)
     {
+
         if (HasPicked) return;
-         //if(player.Object.HasStateAuthority)
-            _renderer.enabled = false;
+        //if(player.Object.HasStateAuthority)
+        // _renderer.enabled = false;
         HasPicked = true;
 
+
     }
-    public void PickedHasChanged()
-    {
-        /*   //  if (Object.HasStateAuthority)
-          Debug.Log("Changing render");
-          _renderer.enabled = false;
-          // Runner.Despawn(Object); */
-    }
+
 }
