b59c7931d0d51c000ca5ae59f48e12fe357462c9
ural89 <uralkemal@gmail.com>
2023-11-24 15:53:25 +0300

    updating line correctly

--- Assets/Scripts/Player/PlayerMovementHandler.cs
+++ Assets/Scripts/Player/PlayerMovementHandler.cs
@@ -10,6 +10,8 @@ public class PlayerMovementHandler : NetworkBehaviour
     private KCC kcc;
     [Networked] private Vector3 currentPosition { get; set; }
     private Vector3 moveDelta;
+    private float initialAngleToShip;
+    private Ship ship;
     private void Awake()
     {
 
@@ -18,6 +20,7 @@ public class PlayerMovementHandler : NetworkBehaviour
     }
     public override void Spawned()
     {
+        ship = FindObjectOfType<Ship>();
         currentPosition = transform.position;
     }
     public void SetCanMove(bool canMove)
@@ -25,7 +28,7 @@ public class PlayerMovementHandler : NetworkBehaviour
         kcc.SetInputDirection(Vector3.zero);
         this.canMove = canMove;
     }
-  
+
     public override void FixedUpdateNetwork()
     {
         moveDelta = Vector3.zero;
@@ -51,9 +54,29 @@ public class PlayerMovementHandler : NetworkBehaviour
 
     private void UpdateMovementToShipPosition() //TODO: carry this to late update
     {
-        kcc.SetPosition(currentPosition + ShipMovement.MoveDelta);
-    }
+        Vector3 playerToShipCenter = transform.position - ship.transform.position;
 
+        // Calculate the angle between the forward direction of the ship and the player-to-ship-center vector
+        float angle = Vector3.SignedAngle(Vector3.forward, playerToShipCenter.normalized, Vector3.up);
+
+        // Incorporate the ship's rotation
+        angle += ShipMovement.rotationDegree;
+
+        // Convert the angle to radians
+        float angleInRadians = Mathf.Deg2Rad * angle;
+
+        // Calculate the new player position based on ship's position, rotated angle, and distance
+        float distanceFromShip = playerToShipCenter.magnitude;
+        Vector3 newPosition = ShipMovement.MoveDelta +
+            new Vector3(Mathf.Sin(angleInRadians), 0, Mathf.Cos(angleInRadians)) * distanceFromShip;
+
+        Debug.Log("angle " + angle + " Distance to ship " + distanceFromShip);
+        Debug.DrawLine(newPosition, newPosition + Vector3.up * 100, Color.white, 0.5f);
+
+        // Set the player's position using KCC
+        kcc.SetPosition(currentPosition + ShipMovement.MoveDelta);
+
+    }
 
 }
 
--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -2,18 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using Fusion;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ShipMovement : NetworkBehaviour
 {
     public static Vector3 MoveDelta;
+    public static float rotationDegree;
     private NetworkRigidbody rb;
     private void Awake()
     {
         rb = GetComponent<NetworkRigidbody>();
     }
+    public override void FixedUpdateNetwork()
+    {
+        rotationDegree = transform.eulerAngles.y;
+        RotateShip(1);
+    }
+ 
     public void UpdateInput(Vector2 movementInput)
     {
-        MoveDelta = new Vector3(movementInput.x, 0, movementInput.y) * Runner.DeltaTime * 5;
+        // Calculate movement in the forward direction
+        MoveDelta = new Vector3(0, 0, movementInput.y) * Runner.DeltaTime * 5;
+
+        // Move the ship
         rb.Rigidbody.MovePosition(transform.position + MoveDelta);
+    
+        RotateShip(movementInput.x);
+    }
+
+    private void RotateShip(float movementInput)
+    {
+        // Calculate rotation based on the input
+        float rotationAngle = movementInput * Runner.DeltaTime * 50; // Adjust the multiplier for rotation speed
+
+        // Create a rotation quaternion
+        Quaternion rotation = Quaternion.Euler(0, rotationAngle, 0);
+
+        // Apply rotation to the rigidbody
+        rb.Rigidbody.MoveRotation(rb.Rigidbody.rotation * rotation);
     }
 }
