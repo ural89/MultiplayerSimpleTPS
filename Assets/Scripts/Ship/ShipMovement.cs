b279fd5379853882516f1a66158cb6af1312910f
ural89 <uralkemal@gmail.com>
2023-11-27 15:39:39 +0300

    nothung

--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -22,10 +22,7 @@ public class ShipMovement : NetworkBehaviour
  
     public void UpdateInput(Vector2 movementInput)
     {
-        // Calculate movement in the forward direction
         MoveDelta = new Vector3(0, 0, movementInput.y) * Runner.DeltaTime * 5;
-
-        // Move the ship
         rb.Rigidbody.MovePosition(transform.position + MoveDelta);
     
         RotateShip(movementInput.x);
@@ -33,13 +30,9 @@ public class ShipMovement : NetworkBehaviour
 
     private void RotateShip(float movementInput)
     {
-        // Calculate rotation based on the input
-        float rotationAngle = movementInput * Runner.DeltaTime * 50; // Adjust the multiplier for rotation speed
 
-        // Create a rotation quaternion
+        float rotationAngle = movementInput * Runner.DeltaTime * 50; // Adjust the multiplier for rotation speed 
         Quaternion rotation = Quaternion.Euler(0, rotationAngle, 0);
-
-        // Apply rotation to the rigidbody
         rb.Rigidbody.MoveRotation(rb.Rigidbody.rotation * rotation);
     }
 }
